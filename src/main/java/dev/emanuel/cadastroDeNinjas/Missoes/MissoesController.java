716e75847a2654c634836b40bd804da7c7dc66aa
Emanuel <emanuelflavio1402@gmail.com>
2025-04-27 12:17:53 -0300

    feat: add routes controller with mock

--- src/main/java/dev/emanuel/cadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/emanuel/cadastroDeNinjas/Missoes/MissoesController.java
@@ -1,9 +1,44 @@
 package dev.emanuel.cadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
+
+    // POST -- Mandar uma requisição para CRIAR uma missão
+    // Criar uma missão
+    @PostMapping('/criar')
+    public String criarMissao(){
+        return "Criado com sucesso!";
+    }
+
+    // PUT -- Mandar uma requisição para editar uma missão
+    // Atualiza uma missao
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Alterado com sucesso!";
+    }
+
+    // GET -- Mandar uma requisição para mostrar as missoes
+    // Busca todas as missões
+    @GetMapping("/todos")
+    public String mostrarTodasMissoes(){
+        return "Lista de missoes";
+    }
+
+    // GET -- Mandar uma requisição para mostrar as missoes por id
+    //Buscar uma missao por id
+    @GetMapping("/mostrarPorId")
+    public String mostrarPorId(){
+        return "Missao por ID";
+    }
+
+    // DELETE -- Mandar uma requisição para apagar uma missão
+    // Apaga uma missão
+    @DeleteMapping("/apagar")
+    public String deletarMissao(){
+        return "Apagado com sucesso!";
+    }
+
 }
