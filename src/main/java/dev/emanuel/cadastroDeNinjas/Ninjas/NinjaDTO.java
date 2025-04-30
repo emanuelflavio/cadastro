8730e9a2f77654ecd9c726216fd33130634d0da0
Emanuel <emanuelflavio1402@gmail.com>
2025-04-30 12:37:58 -0300

    chore: includes version DTO and mapper for entity ninja

--- src/main/java/dev/emanuel/cadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/emanuel/cadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package dev.emanuel.cadastroDeNinjas.Ninjas;
 
+import dev.emanuel.cadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String ranking;
+    private MissoesModel missoes;
+
+
 }
--- src/main/java/dev/emanuel/cadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/emanuel/cadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,37 @@
 package dev.emanuel.cadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setRanking(ninjaDTO.getRanking());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setRanking(ninjaModel.getRanking());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
+
+
 }
