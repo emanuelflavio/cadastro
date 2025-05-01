4de62a84c899fcbdff8f26ea0d1ee994da2427f0
Emanuel <emanuelflavio1402@gmail.com>
2025-05-01 14:13:49 -0300

    fix: remove imports

--- src/main/java/dev/emanuel/cadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/emanuel/cadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,10 +1,8 @@
 package dev.emanuel.cadastroDeNinjas.Ninjas;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/ninjas")
