2cf75dbb91539cde6ab44e7696c5f72013c4aeb8
Emanuel <emanuelflavio1402@gmail.com>
2025-05-02 19:00:27 -0300

    feat: Implements Ui for table ninjas, create controller with routes ui the registration, deletion, listing, of ninjas

--- src/main/java/dev/emanuel/cadastroDeNinjas/Ninjas/NinjaControllerUi.java
+++ src/main/java/dev/emanuel/cadastroDeNinjas/Ninjas/NinjaControllerUi.java
@@ -1,11 +1,11 @@
 package dev.emanuel.cadastroDeNinjas.Ninjas;
 
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -27,4 +27,37 @@ public class NinjaControllerUi {
         model.addAttribute("ninjas", ninjas);
         return "listarNinjas";  // tem que retornar o nome da pagina que renderiza
     }
+
+    @GetMapping("/apagar/{id}")
+    public String apagarNinjaPorId(@PathVariable Long id){
+      ninjaService.apagarNinjaPorId(id);
+      return "redirect:/ninjas/ui/listar";
+    }
+
+
+    @GetMapping("/listar/{id}")
+    public String listarNinjaPorId(@PathVariable Long id, Model model){
+        NinjaDTO ninja = ninjaService.listarNinjaPorId(id);
+        if(ninja != null){
+            model.addAttribute("ninja", ninja);
+            return "detalhesNinja";
+        }
+        model.addAttribute("mensagem", "Ninja não encontrado!");
+        return "listarNinjas";
+    }
+
+    @GetMapping("/adicionar")
+    public String mostrarFormularioNinja(Model model){
+        model.addAttribute("ninja", new NinjaDTO());
+        return "adicionarNinja";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarNinja(@ModelAttribute NinjaDTO ninjaDTO, RedirectAttributes redirect){
+        ninjaService.criarNinja(ninjaDTO);
+        redirect.addFlashAttribute("mensagem", "Ninja adicionado com sucesso!");
+        return "redirect:/ninjas/ui/listar";
+    }
+
+
 }
