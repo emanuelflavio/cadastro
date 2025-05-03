cabe72dc7b47b8892e258dc93978765837633e96
Emanuel <emanuelflavio1402@gmail.com>
2025-05-03 16:44:01 -0300

    feat: create dto and mapper in class missoes

--- src/main/java/dev/emanuel/cadastroDeNinjas/Missoes/MissoesDTO.java
+++ src/main/java/dev/emanuel/cadastroDeNinjas/Missoes/MissoesDTO.java
@@ -1,4 +1,22 @@
 package dev.emanuel.cadastroDeNinjas.Missoes;
 
+
+import dev.emanuel.cadastroDeNinjas.Ninjas.NinjaDTO;
+import dev.emanuel.cadastroDeNinjas.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissoesDTO {
+
+
+    private long id;
+    private String nomeDaMissao;
+    private rankMissoes rankMissoes;
+    private List<NinjaModel> ninjas;
 }
--- src/main/java/dev/emanuel/cadastroDeNinjas/Missoes/MissoesMapper.java
+++ src/main/java/dev/emanuel/cadastroDeNinjas/Missoes/MissoesMapper.java
@@ -1,4 +1,27 @@
 package dev.emanuel.cadastroDeNinjas.Missoes;
 
+import dev.emanuel.cadastroDeNinjas.Ninjas.NinjaDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO) {
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNomeDaMissao(missoesDTO.getNomeDaMissao());
+        missoesModel.setRankMissoes(missoesDTO.getRankMissoes());
+        missoesModel.setNinja(missoesDTO.getNinjas());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel) {
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNomeDaMissao(missoesModel.getNomeDaMissao());
+        missoesDTO.setRankMissoes(missoesModel.getRankMissoes());
+        missoesDTO.setNinjas(missoesModel.getNinja());
+        return missoesDTO;
+    }
 }
