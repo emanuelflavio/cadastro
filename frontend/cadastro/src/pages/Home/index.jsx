311863b21db691897d5244298987c26ed9cab329
Emanuel <emanuelflavio1402@gmail.com>
2025-06-10 21:13:44 -0300

    feat: add function to create and delete

--- frontend/cadastro/src/pages/Home/index.jsx
+++ frontend/cadastro/src/pages/Home/index.jsx
@@ -1,7 +1,9 @@
 import './style.css'
 import Lixo from '../../assets/lixo.png'
 import api from '../../service/api'
-import { useEffect, useState } from 'react'
+import { useEffect, useState,useRef } from 'react'
+
+// useRef
 
 
 
@@ -9,10 +11,33 @@ function Home() {
 
   const [users, setUsers] = useState([])
 
+  const inputNome = useRef()
+  const inputEmail = useRef()
+  const inputImgUrl = useRef()
+  const inputIdade = useRef()
+  const inputRank = useRef()
+
   async function getUsers(){
     const usersFromApi = await api.get('/ninjas/listar')
     setUsers(usersFromApi.data)
     console.log(users)
+  }
+
+    async function createUsers(){
+    const usersFromApi = await api.post('/ninjas/criar',{
+      nome: inputNome.current.value,
+      email: inputEmail.current.value,
+      imgUrl: inputImgUrl.current.value,
+      idade: inputIdade.current.value,
+      ranking: inputRank.current.value
+
+    })
+    getUsers()
+  }
+
+    async function deleteUsers(id){
+    await api.delete(`/ninjas/apagar/${id}`)
+    getUsers()
   }
 
   useEffect(() => {
@@ -26,12 +51,12 @@ function Home() {
     <div className="container">
       <form action="">
         <h1>Cadastro</h1>
-        <input placeholder="Nome" name="nome" type="text" />
-        <input placeholder="Email" name="email" type="email" />
-        <input placeholder="imgUrl" name="imagem" type="text" />
-        <input placeholder="Idade" name="idade" type="number" />
-        <input placeholder="Ranking" name="ranking" type="text" />
-        <button>Cadastrar</button>
+        <input placeholder="Nome" name="nome" type="text" ref={inputNome} />
+        <input placeholder="Email" name="email" type="email" ref={inputEmail}/>
+        <input placeholder="imgUrl" name="imagem" type="text" ref={inputImgUrl}/>
+        <input placeholder="Idade" name="idade" type="number" ref={inputIdade}/>
+        <input placeholder="Ranking" name="ranking" type="text" ref={inputRank}/>
+        <button type="button" onClick={createUsers}>Cadastrar</button>
       </form>
 
       {users.map(user => (
@@ -42,7 +67,7 @@ function Home() {
             <p>Email: <span>{user.email}</span></p>
           </div>
           <div>
-            <button><img src={Lixo} /></button>
+            <button onClick={() => deleteUsers(user.id)}><img src={Lixo} /></button>
           </div>
         </div>
       ))}
